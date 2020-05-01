994bba8ae99ccbbf91483e13d9babe1667f1e97b
henrinet <henrykan288@gmail.com>
2020-05-01 12:17:53 +0800

    1

--- routes/users.js
+++ routes/users.js
@@ -5,15 +5,15 @@ var router = Router();
 /* GET users listing. */
 router.get('/', async (req, res, next)=> {
   try{
-    const users = await users.find()
+    const users = await UsersModel.find()
     res.json(users)
   }catch(err){
     res.status(500).json({message:err.message})
   }
 });
 
-router.get('/:id', (req,res)=>{
-  res.send('');
+router.get('/:id', getUsers, (req,res)=>{
+  res.send(res.users);
 });
 
 router.post('/', async (req,res)=>{
@@ -32,12 +32,47 @@ router.post('/', async (req,res)=>{
 
 });
 
-router.patch('/:id', (req,res)=>{
-
+router.patch('/:id', getUsers, async (req,res)=>{
+  if(req.body.name!=null){
+    const tempName = res.users.name
+    res.users.name=req.body.name
+    res.json({message: 'Name: '+tempName+' is changed to '+res.users.name})
+  }
+  if(req.body.email!=null){
+    const tempEmail = res.users.email
+    res.users.email=req.body.email
+    res.json({message: 'Email: '+tempEmail+' is changed to '+res.users.email})
+  }
+  if(req.body.password!=null){
+    const tempPassword = res.users.password
+    res.users.password=req.body.password
+    res.json({message: 'Password: '+tempPassword+' is changed to '+res.users.password})
+  }
+  try{
+    const updatedUsers = await res.users.save()    
+    res.json(updatedUsers)
+  }catch(err){res.status(400).json({message: err.message})}
 });
 
-router.delete('/:id', (req,res)=>{
-  
+router.delete('/:id', getUsers, async (req,res)=>{
+  try{
+    var userName = res.users.name;
+    await res.users.remove()
+    res.json({message: 'The user '+ userName + ' is deleted!'})
+  }catch(err){
+    res.status(500).json({message: err.message})
+  }
 });
+
+async function getUsers(req, res, next){
+  try{
+    var users = await UsersModel.findById(req.params.id)
+    if(users==null){return res.status(404).json({message: "no such user"})}
+  }catch(err){
+    return res.status(500).json({message: err.message})
+  }
+  res.users = users
+  next()
+}
 
 export default router;
